8917f8e57fc31ac71746f825096b97e741ae588f
JacobSidhu <jacobsidhu100@gmail.com>
2024-03-09 16:20:15 +0000

    Initial Commit: Added basic Structure of the dart file and declared the class named TheatrePage

---
+++ lib/pages/homeScreen/theatre.dart
@@ -0,0 +1,18 @@
+/// theatre.dart
+///
+/// File 'theatre.dart' contains the UI interface for the application for display
+/// currently playing movies in the theatres, This file serves as the UI page to be used
+/// inside HomeScreen Pageview. It has only one Class named as 'TheatrePage' that
+/// extends with the StatelessWidget.
+
+/// Importing essential packages and uitilities needed for the implementation of this design.
+import 'package:flutter/material.dart'; // Material design package.
+
+class TheatrePage extends StatelessWidget {
+  const TheatrePage({super.key});
+  @override // overridinf abstract build method.
+  // Defining implementation of the build method.
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
