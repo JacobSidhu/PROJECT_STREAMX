e3b0b87ca6caf7878d44c8576bffd6094080abbe
JacobSidhu <jacobsidhu100@gmail.com>
2024-03-11 23:03:08 +0000

    Chore: Provider package was imported to get apply theme

--- lib/main.dart
+++ lib/main.dart
@@ -1,44 +1,35 @@
 /// Importing essesntial packages for the application
 
 import 'package:flutter/material.dart'; // Importing material package.
-
+import 'package:provider/provider.dart';
 import 'package:streamx/pages/homeScreen/home_screen.dart';
-
 import 'package:streamx/utils/theme.dart'; // Importing utilities required for this file.
 
 // Invoking the Main root class.
-void main() {
+void main() async {
   /// This Methods ensures the widget initial bindings.
-  // WidgetsFlutterBinding.ensureInitialized();fl
+  WidgetsFlutterBinding.ensureInitialized();
+  // Creating ThemeProvider instance.
+  ThemeProvider themeProvider = ThemeProvider();
+  // Applying user prefered theme.
+  await themeProvider.applyUserTheme();
+
   /// This 'initializeApp()' of class Firebase ensures the initialization of the
   /// firebase platform based configurations and services for the application
   /// before running the application.
   // await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
-  /// Running the application.
-  runApp(const StreamX());
+  /// Running the application with changeNotifier.
+  runApp(ChangeNotifierProvider(
+      create: (context) => themeProvider, child: const StreamX()));
 }
 
 /// Class [StreamX] is defined to present the main home screen of the app
 /// this is the root class of the application that returns the [MaterialApp()]
 /// widget that acts as the wrapper for the application.
-class StreamX extends StatefulWidget {
-  // Constructor for the StreamX class.
+class StreamX extends StatelessWidget {
+  // Creating class constructor.
   const StreamX({super.key});
-
-  @override
-  StreamXState createState() => StreamXState();
-}
-
-class StreamXState extends State<StreamX> {
   final Duration _themeAnimationDuration = Durations.extralong1;
-  static bool isdarkTheme = true;
-  // toggle function was created to change the theme of the application.
-  void toggleTheme() {
-    setState(() {
-      isdarkTheme = !isdarkTheme;
-    });
-  }
-
   // Theme Change Animation Duration.
   @override
   // uilding context using build funtion.
@@ -49,6 +40,6 @@ class StreamXState extends State<StreamX> {
         home: HomeScreen(),
         themeAnimationDuration: _themeAnimationDuration,
         // Setting up dymanic theme for the application.
-        theme: isdarkTheme ? AppTheme.darkMode : AppTheme.lightMode);
+        theme: Provider.of<ThemeProvider>(context).themeData);
   }
 }
