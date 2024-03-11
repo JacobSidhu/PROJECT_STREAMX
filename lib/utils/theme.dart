4911127acf6fbd1851ccc92146fd7f6d52858a85
JacobSidhu <jacobsidhu100@gmail.com>
2024-03-11 23:24:07 +0000

    chore: class name Apptheme was defined to contains the color scheme and other theme data for the application

--- lib/utils/theme.dart
+++ lib/utils/theme.dart
@@ -16,10 +16,7 @@
 /// Outline Color:
 /// - colors use for oultining the UI components. like textfeild or outlined button etc.
 ///
-import 'package:flutter/material.dart';
-import 'package:google_fonts/google_fonts.dart';
-import 'package:shared_preferences/shared_preferences.dart';
-import 'package:streamx/utils/const_values.dart';
+import 'package:flutter/material.dart'; //
 
 /// This utility class is defined to encapsulate themes and colors of the application.
 /// AppTheme class contains the static members for definining light and dark mode used
@@ -142,109 +139,9 @@ class AppTheme {
           scrim: Color(0xff000000),
           inverseSurface: Color(0xffe0e3e8),
           inversePrimary: Color(0xff28638a)));
-
-  /// This static member of the class contains the textstyle data.
-  /// It returns the ThemeData class that contains textTheme.
-  static ThemeData textTheme({required BuildContext context}) {
-    // List of Different font sizes.
-    const double kTitleLargeSize = 22;
-    const double kTitleMediumSize = 16;
-    const double kTitleSmallSize = 14;
-    const double kBodyLargeSize = 16;
-    const double kBodyMediumSize = 14;
-    const double kBodySmallSize = 12;
-    const double klabellargeSize = 14;
-    const double klabelMediumSize = 12;
-    const double klabelSmallSize = 11;
-    const double kDisplayLargeSize = 32;
-    const double kDisplayMediumSize = 28;
-    const double kDisplaySmallSize = 24;
-    return ThemeData(
-        textTheme: TextTheme(
-            bodyLarge: TextStyle(
-                color: Theme.of(context).colorScheme.onSurface,
-                fontFamily: GoogleFonts.roboto.toString(),
-                fontSize: kBodyLargeSize),
-            bodyMedium: TextStyle(
-                color: Theme.of(context).colorScheme.onSurfaceVariant,
-                fontFamily: GoogleFonts.roboto.toString(),
-                fontSize: kBodyMediumSize),
-            bodySmall: TextStyle(
-              color: Theme.of(context).colorScheme.onBackground,
-              fontSize: kBodySmallSize,
-              fontWeight: FontWeight.w400,
-              fontFamily: GoogleFonts.roboto.toString(),
-            ),
-            displayLarge: TextStyle(
-                color: Theme.of(context).colorScheme.onSurface,
-                fontFamily: GoogleFonts.roboto.toString(),
-                fontSize: kDisplayLargeSize,
-                fontWeight: FontWeight.w400),
-            displayMedium: TextStyle(
-                color: Theme.of(context).colorScheme.onSurface,
-                fontFamily: GoogleFonts.roboto.toString(),
-                fontSize: kDisplayMediumSize,
-                fontWeight: FontWeight.w400),
-            displaySmall: TextStyle(
-                color: Theme.of(context).colorScheme.onSurface,
-                fontFamily: GoogleFonts.roboto.toString(),
-                fontSize: kDisplaySmallSize,
-                fontWeight: FontWeight.w400),
-            labelLarge: TextStyle(
-                color: Theme.of(context).colorScheme.onSurface,
-                fontFamily: GoogleFonts.roboto.toString(),
-                fontWeight: FontWeight.w500,
-                fontSize: klabellargeSize),
-            labelMedium: TextStyle(
-                color: Theme.of(context).colorScheme.onPrimary,
-                fontFamily: GoogleFonts.roboto.toString(),
-                fontSize: klabelMediumSize,
-                fontWeight: FontWeight.w500),
-            labelSmall: TextStyle(
-                color: Theme.of(context).colorScheme.onPrimary,
-                fontFamily: GoogleFonts.roboto.toString(),
-                fontSize: klabelSmallSize,
-                fontWeight: FontWeight.w500),
-            titleSmall: TextStyle(
-                color: Theme.of(context).colorScheme.onSurfaceVariant,
-                fontFamily: GoogleFonts.roboto.toString(),
-                fontSize: kTitleSmallSize,
-                fontWeight: FontWeight.w500),
-            titleLarge: TextStyle(
-                color: Theme.of(context).colorScheme.onSurface,
-                fontFamily: GoogleFonts.roboto.toString(),
-                fontSize: kTitleLargeSize,
-                fontWeight: FontWeight.w400),
-            titleMedium: TextStyle(
-                fontFamily: GoogleFonts.roboto.toString(),
-                fontSize: kTitleMediumSize,
-                fontWeight: FontWeight.w500,
-                color: Theme.of(context).colorScheme.onSurface)));
-  }
-
-  /// This function was defined to store the icon data. and returns the icondata from
-  /// the ThemeData class to be used into the application to design icons.
-  /// Returns:
-  /// [ThemeData] - ThemeData class.
-  /// Parameters:
-  /// [context] - of type BuildContext: this parameter requires function context.
-  static ThemeData iconUnSelected({required BuildContext context}) {
-    // List of icon constant properties.
-    const double kIconSize = 24;
-    const double kFill = 0.0;
-    // Returing Themedata Class.
-    return ThemeData(
-        // Using iconTheme property from the ThemeData Class.
-        iconTheme: IconThemeData(
-            size: kIconSize, // Size of icon.
-            fill: kFill, // Fill of the icon.
-            color: Theme.of(context)
-                .colorScheme
-                .onSurfaceVariant // Color of the Icon.
-            ));
-  }
 }
 
+// This class contains the extra colors used within the application.
 class ExtraColors {
   final Color inverseOnSurface;
   final Color primaryFixed;
@@ -289,38 +186,4 @@ class ExtraColors {
     required this.surfaceContainerHigh,
     required this.surfaceContainerHighest,
   });
-}
-
-class ThemeProvider with ChangeNotifier {
-  ThemeData _themeData = AppTheme.darkMode;
-  ThemeData get themeData => _themeData;
-  set themeData(ThemeData themeData) {
-    _themeData = themeData;
-    notifyListeners();
-  }
-
-  Future<void> applyUserTheme() async {
-    SharedPreferences userSettings = await SharedPreferences.getInstance();
-    if (userSettings.getBool("isDark") == null) {
-      _themeData = AppTheme.darkMode;
-      print("theme setting were null");
-    } else {
-      print("Theme setting were not null");
-      _themeData = userSettings.getBool("isDark")!
-          ? AppTheme.darkMode
-          : AppTheme.lightMode;
-    }
-  }
-
-  // toggleTheme function swtich the theme from drak to light and vice versa.
-  Future<void> toggleTheme() async {
-    SharedPreferences userSettings = await SharedPreferences.getInstance();
-    if (_themeData == AppTheme.lightMode) {
-      themeData = AppTheme.darkMode;
-      userSettings.setBool("isDark", false);
-    } else {
-      themeData = AppTheme.lightMode;
-      userSettings.setBool("isDark", true);
-    }
-  }
 }
