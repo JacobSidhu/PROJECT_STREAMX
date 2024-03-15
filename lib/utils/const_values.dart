15334400ea711e996a0b1970643ab3b933e1be3c
JacobSidhu <jacobsidhu100@gmail.com>
2024-03-15 13:27:31 +0000

    chore: created to store local images

---
+++ lib/utils/const_values.dart
@@ -0,0 +1,15 @@
+//Font Families used with the application
+
+class LocalImages {
+  static String kLogo = 'assets/images/Logo.png';
+  static String ksherlockHolmes = 'assets/images/sherlockHolmes.jpg';
+  static String kPosterPlaceHolder = 'assets/images/unavailablePoster.jpg';
+  static String kBestShowsPoster = 'assets/images/MoneyHeist.jpg';
+  static String kFireCountry = 'assets/images/TvShow.jpg';
+  static String kSimpson = 'assets/images/simpson.jpg';
+  static String kpeopleThumnail1 = 'assets/images/peopleThumnail1.jpg';
+  static String kpeopleThumnail2 = 'assets/images/peopleThumnail2.jpg';
+  static String kpeopleThumnail3 = 'assets/images/peopleThumnail3.jpg';
+  static String kposterThumnail1 = 'assets/images/posterThumnail1.jpg';
+  static String kposterThumnail2 = 'assets/images/posterThumnail2.jpg';
+}
